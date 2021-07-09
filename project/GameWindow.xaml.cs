16649c716d3023a70914c410e36b20e908c0260d
abava00 <60212516+abava00@users.noreply.github.com>
2021-07-10 02:23:04 +0900

    Turn

--- project/GameWindow.xaml.cs
+++ project/GameWindow.xaml.cs
@@ -16,62 +16,82 @@ using System.Windows.Shapes;
 
 namespace project
 {
-    /// <summary>
-    /// GameWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class GameWindow : Window
+  /// <summary>
+  /// GameWindow.xaml の相互作用ロジック
+  /// </summary>
+  public partial class GameWindow : Window
+  {
+    public GameWindow()
     {
-        public GameWindow()
-        {
-            InitializeComponent();
-        }
-
-        private bool _isMouseDown;
-        private Point _startPoint;
-        private Point _currentPoint;
-
-        private void OperationArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _isMouseDown = true;
-            _startPoint = e.GetPosition(OperationArea);
-            e.Handled = true;
-
-        }
-
-        private void OperationArea_MouseLeftButtonUp(object sender, MouseEventArgs e)
-        {
-            _isMouseDown = false;
-            e.Handled = true;
-        }
-
-
-        private void OperationArea_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!_isMouseDown)
-            {
-                return;
-            }
-
-            _currentPoint = e.GetPosition(OperationArea);
-
-            double offsetX = _currentPoint.X - _startPoint.X;
-            double offsetY = _currentPoint.Y - _startPoint.Y;
-
-            Matrix matrix = ((MatrixTransform)Target.RenderTransform).Matrix;
-
-            matrix.Translate(offsetX, offsetY);
-            Target.RenderTransform = new MatrixTransform(matrix);
-
-
-            _startPoint = _currentPoint;
-            e.Handled = true;
-
-        }
-        private void OperationArea_MouseLeave(object sender, MouseEventArgs e)
-        {
-            _isMouseDown = false;
-
-            e.Handled = true;
-        }
+      InitializeComponent();
     }
+
+    private bool _isMouseDown;
+    private Point _startPoint;
+    private Point _currentPoint;
+
+    private void OperationArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      _isMouseDown = true;
+      _startPoint = e.GetPosition(OperationArea);
+      e.Handled = true;
+
+    }
+
+    private void OperationArea_MouseLeftButtonUp(object sender, MouseEventArgs e)
+    {
+      _isMouseDown = false;
+      e.Handled = true;
+    }
+
+
+    private void OperationArea_MouseMove(object sender, MouseEventArgs e)
+    {
+      if (!_isMouseDown)
+      {
+        return;
+      }
+
+      _currentPoint = e.GetPosition(OperationArea);
+
+      double offsetX = _currentPoint.X - _startPoint.X;
+      double offsetY = _currentPoint.Y - _startPoint.Y;
+
+      Matrix matrix = ((MatrixTransform)Target.RenderTransform).Matrix;
+
+      matrix.Translate(offsetX, offsetY);
+      Target.RenderTransform = new MatrixTransform(matrix);
+
+
+      _startPoint = _currentPoint;
+      e.Handled = true;
+
+    }
+    private void OperationArea_MouseLeave(object sender, MouseEventArgs e)
+    {
+      _isMouseDown = false;
+
+      e.Handled = true;
+    }
+
+    int your_turn = 0;
+    int my_turn = 1;
+    int or = 0;
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      or++;
+      if(or%2 == 0)
+      {
+        my_turn++;
+        MyTurn.Content = "Now Turn" + my_turn;
+      }
+      else
+      {
+        your_turn ++;
+        YourTurn.Content = "Now Turn" + your_turn;
+      }
+
+
+    }
+  }
 }
